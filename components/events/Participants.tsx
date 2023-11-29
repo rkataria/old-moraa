a903c783c880d8a03ac93f8f5ffc29a89b1319d5
Niraj Kaushal <nirajkaushal007@gmail.com>
2023-11-29 23:17:31 +0530

    created event details page

--- components/events/Participants.tsx
+++ components/events/Participants.tsx
@@ -1,40 +1,35 @@
-import { createClient } from "@/utils/supabase/server"
-import { cookies } from "next/headers"
-import Link from "next/link"
+interface IParticipant {
+  name?: string
+  email: string
+  role: string
+}
 
-const people = [
+const participants: IParticipant[] = [
   {
-    name: "Lindsay Walton",
-    title: "Front-end Developer",
     email: "lindsay.walton@example.com",
     role: "Member",
   },
 ]
 
-export default async function EventsPage() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
-
-  const { data, error } = await supabase.from("event").select()
-
+export default function Participants() {
   return (
-    <div className="px-4 sm:px-6 lg:px-8 mt-12">
+    <div className="">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
           <h1 className="text-base font-semibold leading-6 text-gray-900">
-            Events
+            Participants
           </h1>
           <p className="mt-2 text-sm text-gray-700">
-            List of all events and their details.
+            A list of all the participants.
           </p>
         </div>
         <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <Link
-            href="/events/new"
+          <button
+            type="button"
             className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Create Event
-          </Link>
+            Add Participant
+          </button>
         </div>
       </div>
       <div className="mt-8 flow-root">
@@ -47,31 +42,19 @@ export default async function EventsPage() {
                     scope="col"
                     className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
                   >
-                    Title
+                    Name
                   </th>
                   <th
                     scope="col"
                     className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                   >
-                    Description
+                    Email
                   </th>
                   <th
                     scope="col"
                     className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                   >
-                    Event Type
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                  >
-                    Start Date
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                  >
-                    End Date
+                    Role
                   </th>
                   <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
                     <span className="sr-only">Edit</span>
@@ -79,36 +62,25 @@ export default async function EventsPage() {
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200">
-                {data?.map((event) => (
-                  <tr key={event.name}>
+                {participants.map((participant) => (
+                  <tr key={participant.email}>
                     <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                      <Link
-                        href={`/events/${event.id}`}
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        {event.name}
-                      </Link>
+                      {participant?.name || "Unknown"}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {event.description}
+                      {participant.email}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {event.type}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {event.start_date}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {event.end_date}
+                      {participant.role}
                     </td>
                     <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                      <Link
-                        href={`/events/${event.id}/slides`}
+                      <a
+                        href="#"
                         className="text-indigo-600 hover:text-indigo-900"
                       >
-                        Add Contents
-                        <span className="sr-only">, {event.name}</span>
-                      </Link>
+                        Remove
+                        <span className="sr-only">, {participant.email}</span>
+                      </a>
                     </td>
                   </tr>
                 ))}
