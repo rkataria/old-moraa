90787d12d106cb56c6c68b5f7102925ec2ebcb7b
Rahul Kataria <rahul.kataria@genleap.co>
2024-01-19 18:13:50 +0000

    fixing route groups

--- app/(create)/(routes)/events/layout.tsx
+++ app/(create)/(routes)/events/layout.tsx
@@ -3,22 +3,20 @@
 import { HeaderComponent } from "@/components/partials/header"
 import { SidebarComponent } from "@/components/partials/sidebar"
 
-
 export default function WorkspaceLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div className>
-      {/* <SidebarComponent />
+    <div className="flex">
+      <SidebarComponent />
       <div className="flex flex-col w-full">
         <HeaderComponent />
         <div className="p-4">
           <main className="relative">{children}</main>
         </div>
-      </div> */}
-      {children}
+      </div>
     </div>
   )
 }
