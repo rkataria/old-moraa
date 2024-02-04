b1f82e42b60c11263a823be6ab9d3ce3c3d5ba12
Bhushan <rushabh.dangore@gmail.com>
2024-02-04 20:25:18 +0530

    Updated login page to scss UI

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,21 @@
 "use client"
 
+import React from "react"
 import { UseQueryProvider } from "@/utils/use-query-provider"
 import { ThemeProvider } from "@/providers/theme-provider"
 import { ModalProvider } from "@/providers/modal-provider"
-import { ChakraProvider } from "@chakra-ui/react"
+import {
+  ChakraProvider,
+} from "@chakra-ui/react"
+import { SaasProvider } from "@saas-ui/react"
+import { supabaseClient } from "@/utils/supabase/client"
+import { AuthProvider } from "@saas-ui/auth"
+import { createAuthService } from "@saas-ui/supabase"
+import Link, { LinkProps } from "next/link"
+
+const NextLink = React.forwardRef<HTMLAnchorElement, LinkProps>(
+  (props, ref) => <Link ref={ref} {...props} />
+)
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -14,11 +26,23 @@ export function Providers({ children }: { children: React.ReactNode }) {
         enableSystem={false}
         storageKey="elearning"
       >
-        <UseQueryProvider>
-          <ChakraProvider>{children}</ChakraProvider>
-
-          <ModalProvider />
-        </UseQueryProvider>
+        <ChakraProvider>
+          <SaasProvider linkComponent={NextLink}>
+            <AuthProvider
+              {...createAuthService(supabaseClient, {
+                loginOptions: {
+                  redirectTo: "/dashboard",
+                  shouldCreateUser: false,
+                },
+              })}
+            >
+              <UseQueryProvider>
+                {children}
+                <ModalProvider />
+              </UseQueryProvider>
+            </AuthProvider>
+          </SaasProvider>
+        </ChakraProvider>
       </ThemeProvider>
     </>
   )
