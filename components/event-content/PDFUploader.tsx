2bd86b1d8bb38839b6983c3277f21e794c8c3700
Bhushan <rushabh.dangore@gmail.com>
2024-02-04 14:16:21 +0530

    Bug fixes

--- components/event-content/PDFUploader.tsx
+++ components/event-content/PDFUploader.tsx
@@ -7,7 +7,11 @@ import { OnDocumentLoadSuccess } from "react-pdf/dist/cjs/shared/types"
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker.js"
 import FilePicker from "../ui/file-picker"
 import { NextPrevButtons } from "../common/NextPrevButtons"
-import { downloadPDFFile, uploadPDFFile } from "@/services/pdf.service"
+import {
+  deletePDFFile,
+  downloadPDFFile,
+  uploadPDFFile,
+} from "@/services/pdf.service"
 import SlideManagerContext from "@/contexts/SlideManagerContext"
 import { Button, Input } from "@chakra-ui/react"
 
@@ -22,6 +26,7 @@ const getFileObjectFromBlob = (fileName: string, blob: Blob) => {
 }
 
 export const PDFUploader = ({ slide }: PDFUploaderProps) => {
+  const isEditMode = !slide.content?.pdfPath
   const [file, setFile] = useState<File | undefined>()
   const [totalPages, setTotalPages] = useState<null | number>(null)
   const [defaultPage, setDefaultPage] = useState<null | number>(null)
@@ -30,16 +35,21 @@ export const PDFUploader = ({ slide }: PDFUploaderProps) => {
     SlideManagerContext
   ) as SlideManagerContextType
 
+  const isLoading = slide.content?.pdfPath && !file
   useEffect(() => {
-    downloadPDFFile(slide.content?.pdfPath).then((data) => {
-      if (data.data) {
-        setFile(getFileObjectFromBlob(slide.content?.pdfPath, data.data))
-      }
-    })
+    if (!file && slide.content?.pdfPath) {
+      downloadPDFFile(slide.content?.pdfPath).then((data) => {
+        if (data.data) {
+          setFile(getFileObjectFromBlob(slide.content?.pdfPath, data.data))
+        }
+      })
+    }
     setDefaultPage(slide.content?.defaultPage || null)
   }, [slide.content?.pdfPath])
 
   const uploadAndSetFile = async (file: File) => {
+    setFile(file)
+    if (slide.content?.pdfPath) await deletePDFFile(slide.content?.pdfPath)
     const { data, error } = await uploadPDFFile(slide.id + `_pdf.pdf`, file)
     if (error) {
       return console.error(error)
@@ -71,7 +81,55 @@ export const PDFUploader = ({ slide }: PDFUploaderProps) => {
 
   return (
     <div className="w-full flex flex-col justify-center items-center px-8 bg-white">
-      {!file ? (
+      {!isEditMode || file ? (
+        <div>
+          {isLoading ? null : (
+            <div>
+              <Document
+                file={file}
+                onLoadSuccess={onDocumentLoadSuccess}
+                className="rounded overflow-y-scroll shadow-lg"
+              >
+                <Page
+                  loading={" "}
+                  pageNumber={selectedPage}
+                  renderAnnotationLayer={false}
+                  renderTextLayer={false}
+                  className="m-2 w-96"
+                />
+              </Document>
+              <div className="m-4 flex justify-between items-center">
+                <div className="flex items-center">
+                  <Input
+                    placeholder="Initial page number"
+                    className="mr-2"
+                    type="number"
+                    value={`${defaultPage}`}
+                    onChange={(e) => {
+                      if (
+                        +e.target.value > 0 &&
+                        +e.target.value <= (totalPages || 0)
+                      )
+                        setDefaultPage(+e.target.value)
+                    }}
+                  />
+                  <Button size="sm" onClick={saveDefaultPageNumber}>
+                    Save
+                  </Button>
+                </div>
+                <NextPrevButtons
+                  onPrevious={() =>
+                    setSelectedPage((pos) => (pos > 1 ? pos - 1 : pos))
+                  }
+                  onNext={() => setSelectedPage((pos) => pos + 1)}
+                  prevDisabled={selectedPage === 1}
+                  nextDisabled={selectedPage === totalPages}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      ) : (
         <div className="h-96 flex justify-center items-center">
           <FilePicker
             hideClearButton
@@ -83,48 +141,6 @@ export const PDFUploader = ({ slide }: PDFUploaderProps) => {
               children: "Upload PDF file",
             }}
           />
-        </div>
-      ) : (
-        <div>
-          <Document
-            file={file}
-            onLoadSuccess={onDocumentLoadSuccess}
-            className="rounded overflow-y-scroll shadow-lg"
-          >
-            <Page
-              loading={" "}
-              pageNumber={selectedPage}
-              renderAnnotationLayer={false}
-              renderTextLayer={false}
-              className="m-2 w-96"
-            />
-          </Document>
-          <div className="m-4 flex justify-between items-center">
-            <div className="flex items-center">
-              <Input
-                placeholder="Initial page number"
-                className="mr-2"
-                type="number"
-                value={`${defaultPage}`}
-                onChange={(e) => {
-                  if (
-                    +e.target.value > 0 &&
-                    +e.target.value <= (totalPages || 0)
-                  )
-                    setDefaultPage(+e.target.value)
-                }}
-              />
-              <Button size="sm" onClick={saveDefaultPageNumber}>Save</Button>
-            </div>
-            <NextPrevButtons
-              onPrevious={() =>
-                setSelectedPage((pos) => (pos > 1 ? pos - 1 : pos))
-              }
-              onNext={() => setSelectedPage((pos) => pos + 1)}
-              prevDisabled={selectedPage === 1}
-              nextDisabled={selectedPage === totalPages}
-            />
-          </div>
         </div>
       )}
     </div>
