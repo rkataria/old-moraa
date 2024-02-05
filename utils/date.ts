b782ba4a5e36551207d43c12d51aadf920347101
bhushan dangore <rushabh.dangore@gmail.com>
2024-02-05 17:40:34 +0000

    Merged in date-format-fixed-for-events-listing (pull request #28)
    fix(Events): Fixed the date format of events listing
    
    * fix(Events): Fixed the date format of events listing
    
    
    Approved-by: Rahul Kataria

--- utils/date.ts
+++ utils/date.ts
@@ -1,3 +1,21 @@
 export const getIsoDateString = (date: string) => {
   return new Date(date).toISOString()
 }
+
+export const getFormattedDate = (
+  date: string,
+  { includeTime = false }: { includeTime?: boolean } = {}
+) => {
+  // Extracting components from ISO date string
+  const isoDate = new Date(date)
+  const year = isoDate.getFullYear()
+  const month = String(isoDate.getMonth() + 1).padStart(2, "0")
+  const day = String(isoDate.getDate()).padStart(2, "0")
+  const hours = String(isoDate.getHours()).padStart(2, "0")
+  const minutes = String(isoDate.getMinutes()).padStart(2, "0")
+
+  // Constructing mm-dd-yyyy format date string
+  const mmddyyyyFormatDateString = `${month}-${day}-${year} ${includeTime ? `${hours}:${minutes}` : ""}`
+
+  return mmddyyyyFormatDateString
+}
