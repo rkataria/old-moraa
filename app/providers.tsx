07b3b80b5dbebeb47785c6a38b693a2fdb7d8cee
Bhushan <rushabh.dangore@gmail.com>
2024-02-06 12:27:21 +0530

    Dependencies fixed

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,13 +5,7 @@ import { UseQueryProvider } from "@/utils/use-query-provider"
 import { ThemeProvider } from "@/providers/theme-provider"
 import { ModalProvider } from "@/providers/modal-provider"
 import { ChakraProvider } from "@chakra-ui/react"
-import { SaasProvider } from "@saas-ui/react"
-import Link, { LinkProps } from "next/link"
 import { UserContextProvider } from "@/hooks/useAuth"
-
-const NextLink = React.forwardRef<HTMLAnchorElement, LinkProps>(
-  (props, ref) => <Link ref={ref} {...props} />
-)
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -23,14 +17,12 @@ export function Providers({ children }: { children: React.ReactNode }) {
         storageKey="elearning"
       >
         <ChakraProvider>
-          <SaasProvider linkComponent={NextLink}>
-            <UserContextProvider>
-              <UseQueryProvider>
-                {children}
-                <ModalProvider />
-              </UseQueryProvider>
-            </UserContextProvider>
-          </SaasProvider>
+          <UserContextProvider>
+            <UseQueryProvider>
+              {children}
+              <ModalProvider />
+            </UseQueryProvider>
+          </UserContextProvider>
         </ChakraProvider>
       </ThemeProvider>
     </>
