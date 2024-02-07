3dd77929830b27ad74dea7b3b1bc6b9225a156e3
Bhushan <rushabh.dangore@gmail.com>
2024-02-07 14:01:32 +0530

    Date format fixed

--- utils/date.ts
+++ utils/date.ts
@@ -9,13 +9,13 @@ export const getFormattedDate = (
   // Extracting components from ISO date string
   const isoDate = new Date(date)
   const year = isoDate.getFullYear()
-  const month = String(isoDate.getMonth() + 1).padStart(2, "0")
+  const month = isoDate.toLocaleString('default', { month: 'short' });
   const day = String(isoDate.getDate()).padStart(2, "0")
   const hours = String(isoDate.getHours()).padStart(2, "0")
   const minutes = String(isoDate.getMinutes()).padStart(2, "0")
 
   // Constructing mm-dd-yyyy format date string
-  const mmddyyyyFormatDateString = `${month}-${day}-${year} ${includeTime ? `${hours}:${minutes}` : ""}`
+  const mmddyyyyFormatDateString = `${month} ${day}, ${year} ${includeTime ? `${hours}:${minutes}` : ""}`
 
   return mmddyyyyFormatDateString
 }
