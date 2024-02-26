00c5d4eed8f96a2256943d61839643b8678dfdfa
Niraj Kaushal <nirajkaushal007@gmail.com>
2024-02-26 16:08:36 +0530

    feat(nextui-migration): migrate all components to nextui

--- components/events/CreateEventModal.tsx
+++ components/events/CreateEventModal.tsx
@@ -1,42 +1,33 @@
-"use client";
+"use client"
 
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  Input,
+  Textarea,
+} from "@nextui-org/react"
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation"
 import { useModal } from "@/stores/use-modal-store"
-import { Textarea } from "@/components/ui/textarea";
-import { useAuth } from "@/hooks/useAuth";
-import { useMutation } from "@tanstack/react-query";
-import { EventService } from "@/services/event.service";
+import { useAuth } from "@/hooks/useAuth"
+import { useMutation } from "@tanstack/react-query"
+import { EventService } from "@/services/event.service"
+import { Button } from "@nextui-org/react"
 
 const formSchema = z.object({
   name: z.string().min(1, {
-    message: "Name is required."
+    message: "Name is required.",
   }),
-  description: z.string().optional()
-});
+  description: z.string().optional(),
+})
 
 export const CreateEventModal = () => {
+  const { register, handleSubmit, formState, reset } = useForm()
   const router = useRouter()
   const { isOpen, onClose, type } = useModal()
   const isModalOpen = isOpen && type === "createEvent"
@@ -50,11 +41,11 @@ export const CreateEventModal = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      description: ""
-    }
-  });
+      description: "",
+    },
+  })
 
-  const isLoading = form.formState.isSubmitting;
+  const isLoading = form.formState.isSubmitting
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     if (!currentUser) return
@@ -69,10 +60,11 @@ export const CreateEventModal = () => {
     return await mutateAsync(event, {
       onSuccess: ({ data }) => {
         if (data) {
+          form.reset()
           onClose()
           router.push(`/events/${data.id}`)
         }
-      }
+      },
     })
   }
 
@@ -82,70 +74,79 @@ export const CreateEventModal = () => {
   }
 
   return (
-    <Dialog open={isModalOpen} onOpenChange={handleClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
-        <DialogHeader className="p-6 flex flex-col items-start bg-primary text-white">
-          <DialogTitle className="text-lg text-center">
-            New learning event
-          </DialogTitle>
-          <DialogDescription className="text-textIndigo">
-          Give your event a name and an optional description to get going!        </DialogDescription>
-        </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <div className="space-y-8 px-6">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel
-                      className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
-                    >
+    <Modal size="2xl" isOpen={isModalOpen} onClose={handleClose}>
+      <ModalHeader>
+        <h2 className="text-lg font-bold">Create a new event</h2>
+        <p className="text-textIndigo">
+          Give your event a name and an optional description to get going!
+        </p>
+      </ModalHeader>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              <h2 className="font-md text-gray-800 font-semibold">New Event</h2>
+              <p className="text-gray-500 text-sm font-normal">
+                Give your event a name and an optional description to get going!
+              </p>
+            </ModalHeader>
+            <ModalBody>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div>
+                  <div>
+                    <label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
                       Name
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        disabled={isLoading}
-                        className="focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                        placeholder="Your awesome course or workshop name goes here"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                    </label>
+                    <Input
+                      disabled={isLoading}
+                      className="focus-visible:ring-0 text-black focus-visible:ring-offset-0 placeholder:text-gray-400"
+                      placeholder="Your awesome course or workshop name goes here"
+                      {...register("name", { required: "Name is required." })}
+                    />
+                    {
+                      //@ts-ignore
+                      form.formState.errors.name && (
+                        <p className="text-destructive text-xs mt-1">
+                          {form.formState.errors.name.message}
+                        </p>
+                      )
+                    }
+                  </div>
 
-              <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel
-                      className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
-                    >
+                  <div className="mt-4">
+                    <label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
                       Description
-                    </FormLabel>
-                    <FormControl>
-                      <Textarea placeholder="This is what your learners would see. You could include high-level learning objectives or brief course overview here"
-                        className="focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <DialogFooter className="bg-gray-100 px-6 py-4">
-              <Button variant="default" disabled={isLoading}>
-                Create
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+                    </label>
+                    <Textarea
+                      placeholder="This is what your learners would see. You could include high-level learning objectives or brief course overview here"
+                      className="focus-visible:ring-0 text-black focus-visible:ring-offset-0 placeholder:text-gray-400"
+                      {...register("description")}
+                    />
+                    {
+                      //@ts-ignore
+                      form.formState.errors.description && (
+                        <p className="text-destructive text-xs mt-1">
+                          {form.formState.errors.description.message}
+                        </p>
+                      )
+                    }
+                  </div>
+                </div>
+                <div className="flex justify-end items-center mt-8 mb-4">
+                  <Button
+                    type="submit"
+                    color="primary"
+                    disabled={isLoading}
+                    isLoading={isLoading}
+                  >
+                    Create
+                  </Button>
+                </div>
+              </form>
+            </ModalBody>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
   )
 }
