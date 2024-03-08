1beb27162fcf1251c61986441dd5c0ade27dc9b1
Niraj Kaushal <nirajkaushal007@gmail.com>
2024-03-08 15:57:55 +0000

    Merged in fix-content-type-icon-size-issue (pull request #87)
    fix(content-type): fix icon size
    
    * fix(content-type): fix icon size

--- components/event-content/ContentTypePicker.tsx
+++ components/event-content/ContentTypePicker.tsx
@@ -36,40 +36,40 @@ export const INTERACTIVE_SLIDE_TYPES = [
 export const contentTypes: IContentType[] = [
   {
     name: 'Title & Description',
-    icon: <IconAlignCenter className="w-11 h-11" />,
+    icon: <IconAlignCenter className="w-full h-full max-w-11 max-h-11" />,
     description:
       'Simple and effective. Suitable for cover pages and section dividers',
     contentType: ContentType.COVER,
   },
   {
     name: 'Google Slides',
-    icon: <IconBrandGoogleDrive className="w-11 h-11" />,
+    icon: <IconBrandGoogleDrive className="w-full h-full max-w-11 max-h-11" />,
     description: 'Empower presentations with seamless Google Slides embed',
     contentType: ContentType.GOOGLE_SLIDES,
   },
   {
     name: 'PDF',
-    icon: <IconBrandAdobe className="w-11 h-11" />,
+    icon: <IconBrandAdobe className="w-full h-full max-w-11 max-h-11" />,
     description: 'Upload and integrate your PDF content as a multi-page slide!',
     contentType: ContentType.PDF_VIEWER,
   },
   {
     name: 'Poll',
-    icon: <IconChartBar className="w-11 h-11" />,
+    icon: <IconChartBar className="w-full h-full max-w-11 max-h-11" />,
     description:
       'Break ice, gauge opinions visually. Dive into dialogue effortlessly.',
     contentType: ContentType.POLL,
   },
   {
     name: 'Reflections',
-    icon: <IconCards className="w-11 h-11" />,
+    icon: <IconCards className="w-full h-full max-w-11 max-h-11" />,
     description:
       'Ignite insights through thoughtful reflection. Share perspectives, spark growth.',
     contentType: ContentType.REFLECTION,
   },
   {
     name: 'Video Embed',
-    icon: <IconVideo className="w-11 h-11" />,
+    icon: <IconVideo className="w-full h-full max-w-11 max-h-11" />,
     description:
       'Embed videos from YouTube, Vimeo, or any other video hosting platform',
     contentType: ContentType.VIDEO_EMBED,
@@ -107,13 +107,12 @@ export function ContentTypePicker({
             <ModalBody className="p-6">
               <div className="w-full">
                 <div className="grid grid-cols-3 justify-start items-center gap-4 scrollbar-none rounded-md">
-                  {contentTypes.map((contentType, index) => (
+                  {contentTypes.map((contentType) => (
                     <div
-                      // eslint-disable-next-line react/no-array-index-key
-                      key={index}
+                      key={contentType.contentType}
                       className="rounded-md  aspect-video cursor-pointer transition-all flex flex-col justify-center items-center gap-2 p-[20px] text-center bg-[#353535] text-white hover:bg-black hover:border-black"
                       onClick={() => onChoose(contentType.contentType)}>
-                      <p>{contentType.icon}</p>
+                      <div className="w-11 h-11">{contentType.icon}</div>
                       <h3 className="font-semibold text-xl">
                         {contentType.name}
                       </h3>
