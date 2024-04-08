fc03be662abaaef584b99d67dd7ea7739a89848e
Rashmil Rajpoot <rashmil.rajpoot@coralmango.com>
2024-04-08 09:51:40 +0000

    Merged in feature/DEV-293-fix-raise-hand (pull request #116)
    feature/DEV-293-fix-raise-hand
    
    * worked on fix raise hand issue
    
    * fixed multiple api call issue on speaking
    
    * updated
    
    * code fixed
    
    * proper namings
    
    * removed unused disable lines
    
    * Merge branch 'develop' into feature/DEV-293-fix-raise-hand
    
    
    Approved-by: Dinesh Gadge

--- components/event-session/RaiseHand.tsx
+++ components/event-session/RaiseHand.tsx
@@ -1,6 +1,7 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
-import { useDyteSelector } from '@dytesdk/react-web-core'
+import { useDyteMeeting, useDyteSelector } from '@dytesdk/react-web-core'
+import { useDebounce } from '@uidotdev/usehooks'
 import { HiOutlineHandRaised } from 'react-icons/hi2'
 
 import { EventSessionContext } from '@/contexts/EventSessionContext'
@@ -8,16 +9,39 @@ import { EventSessionContextType } from '@/types/event-session.type'
 import { cn } from '@/utils/utils'
 
 export function RaiseHand() {
+  const { meeting } = useDyteMeeting()
   const selfParticipant = useDyteSelector((m) => m.self)
-  const { isHost, activeSession, onToggleHandRaised, participant } = useContext(
+  const [selfSpeaker, setSelfSpeaker] = useState('')
+  const { activeSession, onToggleHandRaised, participant } = useContext(
     EventSessionContext
   ) as EventSessionContextType
-
-  if (isHost || !participant) return null
 
   const isHandRaised = activeSession?.data?.handsRaised?.includes(
     selfParticipant.id
   )
+
+  const debouncedSelfSpeaker = useDebounce(selfSpeaker, 200)
+
+  useEffect(() => {
+    if (!debouncedSelfSpeaker) return
+    onToggleHandRaised({
+      handRaise: false,
+      participantId: selfParticipant.id,
+      participantName: selfParticipant.name,
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSelfSpeaker])
+
+  useEffect(() => {
+    const handleActiveSpeaker = (speaker: { peerId: string }) => {
+      if (speaker.peerId === selfParticipant.id && isHandRaised) {
+        setSelfSpeaker(`${speaker.peerId} ${Math.random()}`)
+      }
+    }
+    meeting.participants.on('activeSpeaker', handleActiveSpeaker)
+  }, [meeting.participants, isHandRaised, selfParticipant.id])
+
+  if (!participant) return null
 
   return (
     <button
@@ -37,7 +61,7 @@ export function RaiseHand() {
         }
       )}>
       <HiOutlineHandRaised className="text-2xl" />
-      <p className="text-xs">{isHandRaised ? 'Hand Raised' : 'Raise Hand'}</p>
+      <p className="text-xs">{isHandRaised ? 'Lower hand' : 'Raise Hand'}</p>
     </button>
   )
 }
