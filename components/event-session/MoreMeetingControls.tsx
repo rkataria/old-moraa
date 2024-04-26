8df37783aaf2a2c0ea5e2b813c197c99613c5317
Niraj Kaushal <nirajkaushal007@gmail.com>
2024-04-26 04:43:26 +0000

    Merged in DEV-298-make-the-live-session-ui-google-meet-like (pull request #140)
    feat(meeting-layout): update meeting layout
    
    * feat(meeting-layout): update meeting layout
    
    * fix emoji render issue
    
    * fixed sidebar width issue
    
    * fixed button and bg issue
    
    * fixed image overflow issue
    
    * Merged develop into DEV-298-make-the-live-session-ui-google-meet-like
    
    * replaced setting icon with dots
    
    * Merged develop into DEV-298-make-the-live-session-ui-google-meet-like

---
+++ components/event-session/MoreMeetingControls.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
+import { TbAppsFilled } from 'react-icons/tb'
+
+import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/react'
+
+import { cn } from '@/utils/utils'
+
+export function MoreMeetingControls() {
+  return (
+    <Popover placement="bottom" offset={32}>
+      <PopoverTrigger>
+        <button
+          type="button"
+          onClick={() => {}}
+          style={{
+            backgroundColor:
+              'var(--dyte-controlbar-button-background-color, rgb(var(--dyte-colors-background-1000, 8 8 8)))',
+          }}
+          className={cn(
+            'flex flex-col justify-center items-center gap-[5px] w-14 h-10 rounded-sm hover:bg-[#1E1E1E] text-white'
+          )}>
+          <TbAppsFilled className="text-2xl text-white" />
+        </button>
+      </PopoverTrigger>
+      <PopoverContent className="p-1 bg-black/90 min-w-[500px] aspect-video text-white">
+        <div className="text-small font-bold">More controls coming soon!</div>
+      </PopoverContent>
+    </Popover>
+  )
+}
