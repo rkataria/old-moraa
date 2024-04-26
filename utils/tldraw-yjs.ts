79157bf706d5759cebb43d22c6e0f6ede44b00af
Niraj Kaushal <nirajkaushal007@gmail.com>
2024-04-26 10:18:57 +0530

    fixes

--- utils/tldraw-yjs.ts
+++ utils/tldraw-yjs.ts
@@ -4,20 +4,20 @@ import * as Y from 'yjs'
 
 const VERSION = 1
 
+const HOST_URL = `wss://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co/realtime/v1/websocket/tldraw`
+
 // Create the doc
 export const doc = new Y.Doc()
 
 export const roomID = `y-tldraw-${VERSION}`
 
 // Create a websocket provider
-export const provider = new WebsocketProvider(
-  'wss://demos.yjs.dev',
-  roomID,
-  doc,
-  {
-    connect: true,
-  }
-)
+export const provider = new WebsocketProvider(HOST_URL, roomID, doc, {
+  connect: true,
+  params: {
+    apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  },
+})
 
 // Export the provider's awareness API
 export const { awareness } = provider
