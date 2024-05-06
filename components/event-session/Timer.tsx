b6c973e0ac87f459f7108db1346927346034c519
Sajan Kota <sajan@steinnlabs.com>
2024-05-06 10:08:48 +0530

    added the draggable functionality to the timer widget.

--- components/event-session/Timer.tsx
+++ components/event-session/Timer.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState, useCallback } from 'react'
 
+import Draggable from 'react-draggable'
 import {
   MdOutlineWatchLater,
   MdAdd,
@@ -36,26 +37,26 @@ export function Timer({ collapsePopoverContent, dismissPopover }: TimerProps) {
   ) as EventSessionContextType
 
   // useEffect hooks to handle realtime channel events
-  // useEffect(() => {
-  //   // Event listeners for timer start and stop events
-  //   if (!realtimeChannel) return
-  //   realtimeChannel.on(
-  //     'broadcast',
-  //     { event: 'timer-start-event' },
-  //     ({ payload }) => {
-  //       setRemainingDuration(payload.remainingDuration)
-  //       setIsTimerRunning(true)
-  //     }
-  //   )
-  //   realtimeChannel.on(
-  //     'broadcast',
-  //     { event: 'timer-stop-event' },
-  //     ({ payload }) => {
-  //       setRemainingDuration(payload.remainingDuration)
-  //       setIsTimerRunning(false)
-  //     }
-  //   )
-  // }, [realtimeChannel])
+  useEffect(() => {
+    // Event listeners for timer start and stop events
+    if (!realtimeChannel) return
+    realtimeChannel.on(
+      'broadcast',
+      { event: 'timer-start-event' },
+      ({ payload }) => {
+        setRemainingDuration(payload.remainingDuration)
+        setIsTimerRunning(true)
+      }
+    )
+    realtimeChannel.on(
+      'broadcast',
+      { event: 'timer-stop-event' },
+      ({ payload }) => {
+        setRemainingDuration(payload.remainingDuration)
+        setIsTimerRunning(false)
+      }
+    )
+  }, [realtimeChannel])
 
   useEffect(() => {
     realtimeChannel.on(
@@ -80,6 +81,14 @@ export function Timer({ collapsePopoverContent, dismissPopover }: TimerProps) {
     realtimeChannel.on('broadcast', { event: 'timer-close-event' }, () => {
       setIsOpen(false) // Close the popover in response to the close event
     })
+    realtimeChannel.on(
+      'broadcast',
+      { event: 'timer-update-event' },
+      ({ payload }) => {
+        // Update remaining duration when received from host
+        setRemainingDuration(payload.remainingDuration)
+      }
+    )
   }, [realtimeChannel])
 
   useEffect(() => {
@@ -151,148 +160,134 @@ export function Timer({ collapsePopoverContent, dismissPopover }: TimerProps) {
     setIsOpen(open)
   }
 
+  const setRemainingDurationAndUpdate = (newDuration: number) => {
+    // Send the updated duration to the server
+    realtimeChannel.send({
+      type: 'broadcast',
+      event: 'timer-update-event',
+      payload: { remainingDuration: newDuration },
+    })
+    // Update the remaining duration locally
+    setRemainingDuration(newDuration)
+  }
+
   return (
-    <Popover
-      isOpen={isOpen}
-      onOpenChange={handleOpenChange}
-      classNames={{ content: 'p-0' }}
-      placement="top"
-      offset={15}
-      style={{ width: '350px' }}
-      className="fixed top-0 left-[5%] translate-x-[200%]">
-      <PopoverTrigger>
-        <Button
-          className="flex items-center justify-center gap-2 p-1 rounded-sm hover:bg-[#1E1E1E] text-white focus:outline-none focus:ring-0"
-          type="button"
-          disableRipple
-          style={{
-            backgroundColor:
-              'var(--dyte-controlbar-button-background-color, rgb(var(--dyte-colors-background-1000, 8 8 8)))',
-            width: '120px', // Fixed width
-            height: '50px', // Fixed height to accommodate both text and icon
-          }}
-          onClick={handleTimerButtonClick}>
-          <div
+    <Draggable>
+      <Popover
+        isOpen={isOpen}
+        onOpenChange={handleOpenChange}
+        classNames={{ content: 'p-0' }}
+        placement="top"
+        offset={15}
+        className="fixed top-0 left-[5%] translate-x-[200%] cursor-move w-[300px]">
+        <PopoverTrigger>
+          <Button
+            className="w-[120px] h-[50px] flex items-center justify-center gap-2 p-1 rounded-sm hover:bg-[#1E1E1E] text-white focus:outline-none focus:ring-0"
+            type="button"
+            disableRipple
             style={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            {isTimerRunning ? (
-              <div className="text-md font-extrabold flex justify-center items-center">
-                <span
-                  style={{
-                    width: '24px',
-                    display: 'inline-block',
-                    textAlign: 'center',
-                  }}>
+              backgroundColor:
+                'var(--dyte-controlbar-button-background-color, rgb(var(--dyte-colors-background-1000, 8 8 8)))',
+            }}
+            onClick={handleTimerButtonClick}>
+            <div className="w-[100%] h-[100%] flex items-center justify-center">
+              {isTimerRunning ? (
+                <div className="text-md font-extrabold flex justify-center items-center">
+                  <span className="w-[16px] inline-block text-center">
+                    {zeroPad(Math.floor(remainingDuration / 60), 2)}
+                  </span>
+                  :
+                  <span className="w-[8px] inline-block text-center">
+                    {zeroPad(remainingDuration % 60, 2)}
+                  </span>
+                </div>
+              ) : (
+                <MdOutlineWatchLater className="text-2xl" />
+              )}
+            </div>
+          </Button>
+        </PopoverTrigger>
+
+        <PopoverContent className="rounded-lg p-4 overflow-hidden w-[300px]">
+          <div
+            className={`absolute top-2 right-2 cursor-pointer ${!isHost ? 'opacity-50 cursor-not-allowed' : ''}`}
+            onClick={() => isHost && handleClosePopover()} // Only handle click if isHost is true
+            onKeyDown={(event) => isHost && handleKeyDown(event)} // Only handle key down if isHost is true
+            tabIndex={isHost ? 0 : -1} // Disable tab navigation for non-hosts
+            role="button"
+            aria-label="Close"
+            style={{ pointerEvents: isHost ? 'auto' : 'none' }} // Disallow pointer events if not host account
+          >
+            <MdClose size={20} />
+          </div>
+          <div className="p-4 flex items-center">
+            <div className="flex justify-between items-center">
+              {/* Inside the Button component for decreasing timer duration */}
+              <Button
+                isIconOnly
+                radius="full"
+                disabled={!isHost}
+                onClick={() => {
+                  if (remainingDuration > 15) {
+                    setRemainingDurationAndUpdate(remainingDuration - 15) // Update duration and broadcast the change
+                  }
+                }}
+                className={!isHost ? 'cursor-not-allowed' : ''}>
+                <MdRemove />
+              </Button>
+              <h2 className="m-2 text-md font-extrabold px-2 text-gray-600">
+                <span className="text-4xl inline-block text-center w-[48px]">
                   {zeroPad(Math.floor(remainingDuration / 60), 2)}
-                </span>
+                </span>{' '}
                 :
-                <span
-                  style={{
-                    width: '24px',
-                    display: 'inline-block',
-                    textAlign: 'center',
-                  }}>
+                <span className="w-[16px] inline-block text-center">
                   {zeroPad(remainingDuration % 60, 2)}
                 </span>
-              </div>
-            ) : (
-              <MdOutlineWatchLater className="text-2xl" />
-            )}
+              </h2>
+              {/* Inside the Button component for increasing timer duration */}
+              <Button
+                isIconOnly
+                radius="full"
+                disabled={!isHost}
+                onClick={() => {
+                  setRemainingDurationAndUpdate(remainingDuration + 15) // Update duration and broadcast the change
+                }}
+                className={!isHost ? 'cursor-not-allowed' : ''}>
+                <MdAdd />
+              </Button>
+            </div>
           </div>
-        </Button>
-      </PopoverTrigger>
-
-      <PopoverContent
-        className="rounded-lg p-4 overflow-hidden"
-        style={{ width: '350px' }}>
-        <div
-          className={`absolute top-2 right-2 cursor-pointer ${!isHost ? 'opacity-50 cursor-not-allowed' : ''}`}
-          onClick={() => isHost && handleClosePopover()} // Only handle click if isHost is true
-          onKeyDown={(event) => isHost && handleKeyDown(event)} // Only handle key down if isHost is true
-          tabIndex={isHost ? 0 : -1} // Disable tab navigation for non-hosts
-          role="button"
-          aria-label="Close"
-          style={{ pointerEvents: isHost ? 'auto' : 'none' }} // Disallow pointer events if not host
-        >
-          <MdClose size={20} />
-        </div>
-        <div className="p-4 flex items-center">
-          <div className="flex justify-between items-center">
+          <div className="flex items-center">
             <Button
               isIconOnly
+              size="sm"
               radius="full"
+              color="secondary"
+              className={!isHost ? 'cursor-not-allowed mr-4' : 'mr-4'}
               disabled={!isHost}
               onClick={() => {
-                if (remainingDuration > 15) {
-                  setRemainingDuration((d) => d - 15)
-                }
+                setIsTimerRunning(false)
+                setRemainingDuration(5 * 60)
               }}>
-              <MdRemove />
+              <MdOutlineReplay size={18} fill="gray" />
             </Button>
-            <h2 className="m-2 text-md font-extrabold px-2 text-gray-600">
-              <span
-                className="text-4xl"
-                style={{
-                  width: '48px',
-                  display: 'inline-block',
-                  textAlign: 'center',
-                }}>
-                {zeroPad(Math.floor(remainingDuration / 60), 2)}
-              </span>{' '}
-              :
-              <span
-                style={{
-                  width: '48px',
-                  display: 'inline-block',
-                  textAlign: 'center',
-                }}>
-                {zeroPad(remainingDuration % 60, 2)}
-              </span>
-            </h2>
             <Button
               isIconOnly
+              size="lg"
               radius="full"
-              disabled={!isHost}
-              onClick={() => setRemainingDuration((d) => d + 15)}>
-              <MdAdd />
+              color="primary"
+              onClick={handleTimerToggle}
+              className={!isHost ? 'cursor-not-allowed ml-4' : 'ml-4'}
+              disabled={!isHost}>
+              {!isTimerRunning ? (
+                <MdOutlinePlayArrow size={32} fill="white" />
+              ) : (
+                <MdOutlinePause size="{32}" fill="white" />
+              )}
             </Button>
           </div>
-        </div>
-        <div className="flex items-center">
-          <Button
-            isIconOnly
-            size="sm"
-            radius="full"
-            color="secondary"
-            className="mr-4"
-            disabled={!isHost}
-            onClick={() => {
-              setIsTimerRunning(false)
-              setRemainingDuration(5 * 60)
-            }}>
-            <MdOutlineReplay size={18} fill="gray" />
-          </Button>
-          <Button
-            isIconOnly
-            size="lg"
-            radius="full"
-            color="primary"
-            onClick={handleTimerToggle}
-            className="ml-4"
-            disabled={!isHost}>
-            {!isTimerRunning ? (
-              <MdOutlinePlayArrow size={32} fill="white" />
-            ) : (
-              <MdOutlinePause size="{32}" fill="white" />
-            )}
-          </Button>
-        </div>
-      </PopoverContent>
-    </Popover>
+        </PopoverContent>
+      </Popover>
+    </Draggable>
   )
 }
