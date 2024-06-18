e72602c046eda75584138e15d63a62153201f81d
Niraj Kaushal <nirajkaushal007@gmail.com>
2024-06-18 13:31:41 +0000

    Merged in DEV-503-move-moraaboard-to-liveblocks (pull request #245)
    DEV-503 partykit integration on moraaboard
    
    * partykit integration on moraaboard
    
    * Merged develop into DEV-503-move-moraaboard-to-liveblocks

--- components/event-content/content-types/MoraaBoardEditor.tsx
+++ components/event-content/content-types/MoraaBoardEditor.tsx
@@ -2,33 +2,25 @@
 
 import React, { useContext } from 'react'
 
-import { useDebounce } from '@uidotdev/usehooks'
 import { Tldraw } from 'tldraw'
 
-import { ContentLoading } from '@/components/common/ContentLoading'
 import { EventContext } from '@/contexts/EventContext'
-import { useYjsStoreSupabase } from '@/hooks/useYjsStoreSupabase'
+import { useYjsStore } from '@/hooks/useYjsStore'
 import { EventContextType } from '@/types/event-context.type'
 import { IFrame } from '@/types/frame.type'
 
 type MoraaBoardFrame = IFrame
 
 export function MoraaBoardEditor({ frame }: { frame: MoraaBoardFrame }) {
-  const roomId = `edit-moraa-board-${frame.id}`
   const { preview } = useContext(EventContext) as EventContextType
-  const store = useYjsStoreSupabase({
-    roomId,
-    frameId: frame.id,
+  // const store = useLiveblocksStore()
+  const store = useYjsStore({
+    roomId: frame.id,
+    hostUrl: process.env.NEXT_PUBLIC_PARTYKIT_HOST_URL,
   })
-  const debouncedStatus = useDebounce(store.status, 2000)
 
   return (
     <div className="relative w-full h-full flex flex-col justify-center items-center rounded-md overflow-hidden z-[0]">
-      {debouncedStatus !== 'synced-remote' && (
-        <div className="absolute left-0 top-0 w-full h-full flex justify-center items-center">
-          <ContentLoading />
-        </div>
-      )}
       <Tldraw
         // persistenceKey={roomId}
         autoFocus
