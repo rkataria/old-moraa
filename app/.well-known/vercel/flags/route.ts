9ea8108f1cb1a9ca9bbb38f16ad94c1615ad0590
Rashmil Rajpoot <rashmil.rajpoot@coralmango.com>
2024-07-11 15:43:25 +0530

    show feature flags on toolbar

--- app/.well-known/vercel/flags/route.ts
+++ app/.well-known/vercel/flags/route.ts
@@ -1,20 +1,29 @@
 import { NextResponse, type NextRequest } from 'next/server'
 import { verifyAccess, type ApiData } from '@vercel/flags'
+import { useFlags } from '../../../../flags/client'
 
 export async function GET(request: NextRequest) {
   const access = await verifyAccess(request.headers.get('Authorization'))
   if (!access) return NextResponse.json(null, { status: 401 })
+  const { flags } = useFlags()
+  console.log('flags')
 
-  return NextResponse.json<ApiData>({
-    definitions: {
-      newFeature: {
-        description: 'Controls whether the new feature is visible',
-        origin: 'https://example.com/#new-feature',
+  function getDefinitions(_flags) {
+    const result = {}
+    for (const [key, value] of Object.entries(_flags)) {
+      result[key] = {
         options: [
           { value: false, label: 'Off' },
           { value: true, label: 'On' },
         ],
-      },
-    },
+      }
+    }
+
+    return result
+  }
+  console.log('_flags', getDefinitions(flags))
+
+  return NextResponse.json<ApiData>({
+    definitions: getDefinitions(flags),
   })
 }
