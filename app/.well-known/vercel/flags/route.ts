dfcb586ecc6d5221177ebed63bee8e2ce0bc8e29
Rashmil Rajpoot <rashmil.rajpoot@coralmango.com>
2024-07-11 15:55:14 +0530

    some fix to show feature flags on toolbar

--- app/.well-known/vercel/flags/route.ts
+++ app/.well-known/vercel/flags/route.ts
@@ -1,29 +1,23 @@
 import { NextResponse, type NextRequest } from 'next/server'
 import { verifyAccess, type ApiData } from '@vercel/flags'
-import { useFlags } from '../../../../flags/client'
+import { getFlags } from '../../../../flags/server'
 
 export async function GET(request: NextRequest) {
   const access = await verifyAccess(request.headers.get('Authorization'))
   if (!access) return NextResponse.json(null, { status: 401 })
-  const { flags } = useFlags()
-  console.log('flags')
+  const flagsFromHappyKit = getFlags({})
+  console.log('flagsFromHappyKit', flagsFromHappyKit)
 
-  function getDefinitions(_flags) {
-    const result = {}
-    for (const [key, value] of Object.entries(_flags)) {
-      result[key] = {
+  return NextResponse.json<ApiData>({
+    definitions: {
+      newFeature: {
+        description: 'Controls whether the new feature is visible',
+        origin: 'https://example.com/#new-feature',
         options: [
           { value: false, label: 'Off' },
           { value: true, label: 'On' },
         ],
-      }
-    }
-
-    return result
-  }
-  console.log('_flags', getDefinitions(flags))
-
-  return NextResponse.json<ApiData>({
-    definitions: getDefinitions(flags),
+      },
+    },
   })
 }
