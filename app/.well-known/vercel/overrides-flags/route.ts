e401cad88b329a08d385b78af57b456aff4870da
Rashmil Rajpoot <rashmil.rajpoot@coralmango.com>
2024-07-12 12:32:17 +0530

    fixed issues

--- app/.well-known/vercel/overrides-flags/route.ts
+++ app/.well-known/vercel/overrides-flags/route.ts
@@ -5,8 +5,8 @@ import { FlagOverridesType, decrypt } from '@vercel/flags'
 import { cookies } from 'next/headers'
 
 export async function GET(request: NextRequest) {
-  const access = await verifyAccess(request.headers.get('Authorization'))
-  if (!access) return NextResponse.json(null, { status: 401 })
+  //   const access = await verifyAccess(request.headers.get('Authorization'))
+  //   if (!access) return NextResponse.json(null, { status: 401 })
   const overrideCookie = cookies().get('vercel-flag-overrides')?.value
   const overrides = overrideCookie
     ? await decrypt<FlagOverridesType>(overrideCookie)
