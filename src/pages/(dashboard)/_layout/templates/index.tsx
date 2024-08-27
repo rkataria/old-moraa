bffac043e7d6033f31b92bb292330713679b4287
Niraj Kaushal <nirajkaushal007@gmail.com>
2024-08-27 16:08:39 +0530

    fix lint

--- src/pages/(dashboard)/_layout/templates/index.tsx
+++ src/pages/(dashboard)/_layout/templates/index.tsx
@@ -6,6 +6,8 @@ import { beforeLoad } from '@/utils/before-load'
 
 export const Route = createFileRoute('/(dashboard)/_layout/templates/')({
   component: () => <TemplatesPage />,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   beforeLoad,
 })
 
