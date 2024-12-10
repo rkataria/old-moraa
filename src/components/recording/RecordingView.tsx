e507b833d04f85d31ed6be0029ce6d557d024d70
Niraj Kaushal <nirajkaushal007@gmail.com>
2024-12-10 08:07:49 +0000

    Merged in DEV-813-enable-recording-of-moraa-presentation-9-dec-niraj (pull request #496)
    recording sample test
    
    * recording sample test
    
    * Merge branch 'develop' into DEV-813-enable-recording-of-moraa-presentation-9-dec-niraj

---
+++ src/components/recording/RecordingView.tsx
@@ -0,0 +1,76 @@
+import { useEffect } from 'react'
+
+import {
+  DyteMixedGrid,
+  DyteParticipantsAudio,
+  DyteSimpleGrid,
+} from '@dytesdk/react-ui-kit'
+import { useDyteMeeting, useDyteSelector } from '@dytesdk/react-web-core'
+
+const TARGET_PRESET = 'LEAD'
+
+export function RecordingView() {
+  const { meeting } = useDyteMeeting()
+
+  const joinedParticipants = useDyteSelector((m) =>
+    m.participants.joined.toArray()
+  )
+
+  const targetParticipants = joinedParticipants.filter(
+    (participant) => participant.presetName === TARGET_PRESET
+  )
+
+  const screensharedParticipants = useDyteSelector((m) =>
+    m.participants.joined.toArray().filter((p) => p.screenShareEnabled)
+  )
+
+  const hasScreenshare = screensharedParticipants.length > 0
+
+  useEffect(() => {
+    // Ideally there should be just one participant with the preset name "LEAD"
+    // Comment out if you don't want to pin the peer
+    // for (const participant of targetParticipants) {
+    //   participant.pin();
+    // }
+  }, [targetParticipants])
+
+  return (
+    <main
+      style={{
+        display: 'flex',
+        position: 'relative',
+        paddingTop: '0rem',
+        flexWrap: 'wrap',
+        flexShrink: '0',
+        justifyContent: 'center',
+        alignContent: 'center',
+        width: '100vw',
+        height: '100vh',
+      }}>
+      {hasScreenshare ? (
+        <DyteMixedGrid
+          participants={targetParticipants}
+          pinnedParticipants={targetParticipants}
+          screenShareParticipants={screensharedParticipants}
+          plugins={[]}
+          meeting={meeting}
+          style={{
+            width: '100vw',
+            height: '100vh',
+          }}
+        />
+      ) : (
+        <DyteSimpleGrid
+          meeting={meeting}
+          participants={targetParticipants}
+          style={{
+            width: '100vw',
+            height: '100vh',
+          }}
+        />
+      )}
+
+      <DyteParticipantsAudio meeting={meeting} />
+    </main>
+  )
+}
---
+++ src/pages/event-session/$eventId/record/index.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { useEffect } from 'react'
+
+import { DyteProvider, useDyteClient } from '@dytesdk/react-web-core'
+import { DyteRecording } from '@dytesdk/recording-sdk'
+import { createFileRoute, useParams, useRouter } from '@tanstack/react-router'
+
+import { RecordingView } from '@/components/recording/RecordingView'
+import { supabaseClient } from '@/utils/supabase/client'
+
+export const Route = createFileRoute('/event-session/$eventId/record/')({
+  component: RecordPage,
+})
+
+export function RecordPage() {
+  const [meeting, initMeeting] = useDyteClient()
+  const { eventId } = useParams({
+    strict: false,
+  })
+  const router = useRouter()
+  const searchParams = router.latestLocation.search as {
+    authToken: string
+  }
+  const { authToken } = searchParams
+
+  useEffect(() => {
+    async function loginAnonymously() {
+      const { error } = await supabaseClient.auth.signInWithPassword({
+        email: 'recording-bot@yopmail.com',
+        password: 'letmepass',
+      })
+
+      if (error) {
+        console.error(error)
+
+        return false
+      }
+
+      return true
+    }
+    async function setupDyteMeeting() {
+      const recordingSDK = new DyteRecording({
+        devMode: false,
+      })
+      const meetingObj = await initMeeting({
+        authToken,
+        defaults: {
+          video: false,
+          audio: false,
+        },
+      })
+      if (!meetingObj) {
+        return
+      }
+      await recordingSDK.init(meetingObj)
+      recordingSDK.startRecording()
+    }
+    if (!meeting) {
+      loginAnonymously().then((loggedIn) => {
+        if (loggedIn) {
+          setupDyteMeeting()
+        }
+      })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [meeting, authToken])
+
+  if (!authToken) {
+    return <div>Auth token not found</div>
+  }
+
+  if (!eventId) return <div>Event ID not found</div>
+
+  return (
+    <DyteProvider value={meeting} fallback={<div>Loading...</div>}>
+      <RecordingView />
+    </DyteProvider>
+  )
+}
